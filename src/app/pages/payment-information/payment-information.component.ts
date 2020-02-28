b2f269bdc6260b90b4d88ccaa75aac497a8807b6
Nandun Thilina Bandara <ntbandara3@hotmail.com>
2020-02-28 14:52:49 +0530

    Added credit card details view

--- src/app/pages/payment-information/payment-information.component.ts
+++ src/app/pages/payment-information/payment-information.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentInformationComponent implements OnInit {
 
+  public cardNumber: string;
+  public cardHoldersName: string;
+  public expiry: string;
+  public cvc: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onUpdateCardNumber() {
+    this.cardNumber = this.cardNumber.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
   }
 
 }
