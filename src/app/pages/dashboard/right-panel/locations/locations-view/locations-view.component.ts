44d97c6f596bb88e1d01eceee5df4f1f5febb16e
Nandun Bandara <ntbandara3@hotmail.com>
2020-07-26 05:56:07 +0530

    Sample data

--- src/app/pages/dashboard/right-panel/locations/locations-view/locations-view.component.ts
+++ src/app/pages/dashboard/right-panel/locations/locations-view/locations-view.component.ts
@@ -16,28 +16,62 @@ export class LocationsViewComponent implements OnInit {
 
   locations: MatTableDataSource<any>;
 
+  private data = [
+    {
+      name: 'Home Sweet Home',
+      address: '123, BumbleBee Street, Colombo 2',
+      verified: true,
+    },
+    {
+      name: 'My Office',
+      address: '45D, Gamunupura Road, Nugegoda',
+      verified: false,
+    },
+    {
+      name: 'My Apartment',
+      address: '45C/3, Kaitland Street, Colombo 7',
+      verified: true,
+    },
+    {
+      name: 'Building 1',
+      address: '123, Colombo Street, Kandy',
+      verified: false,
+    },
+    {
+      name: 'My Office - Kandy Branch',
+      address: '33/4, Dutugemunu Weediya, Kandy',
+      verified: false,
+    },
+    {
+      name: 'My Apartment - Kandy',
+      address: '3/4C, Awesome Residencies, Katugasthota, Kandy',
+      verified: true,
+    }
+  ];
+
   constructor(
     private locationService: LocationService,
     private authService: AuthService
   ) { }
 
   ngOnInit() {
-    firebase.auth().currentUser.getIdTokenResult().then(idTokenResult => {
-      console.log(idTokenResult);
-      if (idTokenResult.claims.userType === 1) {
-        this.locationService.getLocationsByUser(firebase.auth().currentUser.uid).subscribe((response: APIResponse) => {
-          this.locations = new MatTableDataSource<any>(response.data);
-        });
-      } else if (idTokenResult.claims.userType === 2) {
-        this.locationService.getLocationsByCouncil(idTokenResult.claims.councilId).subscribe((response: APIResponse) => {
-          this.locations = new MatTableDataSource<any>(response.data);
-        });
-      } else if (idTokenResult.claims.userType === 3) {
-        this.locationService.getAllLocations().subscribe((response: APIResponse) => {
-          this.locations = new MatTableDataSource<any>(response.data);
-        });
-      }
-    });
+    // firebase.auth().currentUser.getIdTokenResult().then(idTokenResult => {
+    //   console.log(idTokenResult);
+    //   if (idTokenResult.claims.userType === 1) {
+    //     this.locationService.getLocationsByUser(firebase.auth().currentUser.uid).subscribe((response: APIResponse) => {
+    //       this.locations = new MatTableDataSource<any>(response.data);
+    //     });
+    //   } else if (idTokenResult.claims.userType === 2) {
+    //     this.locationService.getLocationsByCouncil(idTokenResult.claims.councilId).subscribe((response: APIResponse) => {
+    //       this.locations = new MatTableDataSource<any>(response.data);
+    //     });
+    //   } else if (idTokenResult.claims.userType === 3) {
+    //     this.locationService.getAllLocations().subscribe((response: APIResponse) => {
+    //       this.locations = new MatTableDataSource<any>(response.data);
+    //     });
+    //   }
+    // });
+    this.locations = new MatTableDataSource(this.data);
   }
 
 }
