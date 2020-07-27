68019c9a7968bf31ac9e6cf73183e41df73c8d69
Nandun Bandara <ntbandara3@hotmail.com>
2020-07-28 05:19:47 +0530

    Invoice ui change and bins table

--- src/app/pages/dashboard/right-panel/bins/bins-table/bins-table.component.ts
+++ src/app/pages/dashboard/right-panel/bins/bins-table/bins-table.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 const ELEMENT_DATA = [
-  { name: 'Stupid bin', description: 'To dumb idiots', capacity: '20l' }
+  { name: 'Bin A', description: 'Recyclable Material', location: 'Home Sweet Home', capacity: '20l' },
+  { name: 'Bin B', description: 'Food', location: 'Home Sweet Home', capacity: '30l' },
+  { name: 'Bin C', description: 'Plastic', location: 'Home Sweet Home', capacity: '10l' },
+  { name: 'Bin A', description: 'Recyclable Material', location: 'My Apartment', capacity: '10l' },
+  { name: 'Bin B', description: 'Food', location: 'My Apartment', capacity: '10l' },
+  { name: 'Bin C', description: 'Plastic', location: 'My Apartment', capacity: '10l' }
 ];
 
 @Component({
@@ -16,7 +21,7 @@ export class BinsTableComponent implements OnInit {
   ngOnInit() {
   }
 
-  displayedColumns: string[] = ['name', 'description', 'capacity', 'action'];
+  displayedColumns: string[] = ['name', 'description', 'location', 'capacity', 'action'];
   dataSource = ELEMENT_DATA;
 
 }
