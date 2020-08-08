74b51324ec5aa53575c865fba380dd9cf3759c98
Nandun Bandara <ntbandara3@hotmail.com>
2020-08-08 11:06:47 +0530

    Organization view

--- src/app/pages/dashboard/right-panel/organizations/organizations-view/organizations-view.component.ts
+++ src/app/pages/dashboard/right-panel/organizations/organizations-view/organizations-view.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationsViewComponent implements OnInit {
 
+  displayedColumns: string[] = ['name', 'description', 'status', 'actions'];
+
   constructor() { }
 
   ngOnInit() {
