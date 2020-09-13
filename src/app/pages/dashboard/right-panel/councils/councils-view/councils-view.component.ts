e32e6a74bdb418ed057509618a9ae80e7a137de5
Nandun Bandara <ntbandara3@hotmail.com>
2020-09-14 00:59:17 +0530

    Added councils to admin

--- src/app/pages/dashboard/right-panel/councils/councils-view/councils-view.component.ts
+++ src/app/pages/dashboard/right-panel/councils/councils-view/councils-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CouncilsService } from 'src/app/services/councils.service';
+import { APIResponse } from 'src/app/helpers/api-response';
+import { MatTableDataSource } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-councils-view',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CouncilsViewComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'email', 'description', 'actions'];
+
+  organizations: MatTableDataSource<any>;
+
+  constructor(
+    private councilService: CouncilsService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.getCouncils();
+  }
+
+  private getCouncils() {
+    return this.councilService.getCouncils().subscribe((response: APIResponse) => {
+      this.organizations = new MatTableDataSource<any>(response.data);
+    });
   }
 
 }
--- src/app/pages/dashboard/right-panel/councils/create-council/create-council.component.ts
+++ src/app/pages/dashboard/right-panel/councils/create-council/create-council.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CouncilsService } from 'src/app/services/councils.service';
+import { APIResponse } from 'src/app/helpers/api-response';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-council',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCouncilComponent implements OnInit {
 
-  constructor() { }
+  name;
+  description;
+  email;
+  password;
+
+  constructor(
+    private councilService: CouncilsService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  public createCouncil() {
+    this.councilService.createNewCouncil({
+      name: this.name, description: this.description, 
+      email: this.email, password: this.password
+    }).subscribe((response: APIResponse) => {
+      this.router.navigate(['dashboard/councils']);
+    })
   }
 
 }
