396533689d83bca5a899871981eed6b389bf3e0f
Nandun Bandara <ntbandara3@hotmail.com>
2020-09-17 08:40:17 +0530

    Vehicle create implemented

--- src/app/pages/dashboard/right-panel/vehicles/vehicle-create/vehicle-create.component.ts
+++ src/app/pages/dashboard/right-panel/vehicles/vehicle-create/vehicle-create.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { VehicleService } from 'src/app/services/vehicle.service';
 import { APIResponse } from 'src/app/helpers/api-response';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-vehicle-create',
@@ -9,10 +12,40 @@ import { APIResponse } from 'src/app/helpers/api-response';
 })
 export class VehicleCreateComponent implements OnInit {
 
+  registration_number;
+  model;
+  make;
+  capacity;
+  councilUid;
+
   constructor(
-    private vehicleService: VehicleService
+    private vehicleService: VehicleService,
+    private authService: AuthService,
+    private router: Router,
+    private snackbar: MatSnackBar,
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authService.getIdTokenResult().then(token => {
+      this.councilUid = token.claims.councilId;
+    });
+  }
+
+  save() {
+
+    if (!this.registration_number || !this.model || !this.make || !this.capacity) {
+      return this.snackbar.open('Please enter all required', null, { duration: 3000 });
+    }
+
+    this.vehicleService.createVehicle({ 
+      registration_number: this.registration_number,
+      model: this.model,
+      make: this.make,
+      capacity: this.capacity,
+      councilUid: this.councilUid
+    }).subscribe((response: APIResponse) => {
+      this.router.navigate(['dashboard/vehicles'])
+    })
+  }
 
 }
